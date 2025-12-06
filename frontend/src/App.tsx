cfcd6c278cc5b84f98adce61468408e03883c915
Synix38 <Darrenkzy@gmail.com>
2025-12-06 18:00:38 +0800

    Flight Ops Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import FlightOpsDashboard from './pages/FlightOpsDashboard'
 import './App.css'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <h1>Flight Delay Prediction Dashboard</h1>
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<div>Dashboard - Coming Soon</div>} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<FlightOpsDashboard />} />
+        </Routes>
       </div>
     </Router>
   )
